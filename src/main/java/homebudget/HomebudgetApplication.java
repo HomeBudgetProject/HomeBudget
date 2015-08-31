54c9c8bc3b3214d362fed3812f380b2439954c91
vgotsuliak <973gimf6>
2015-08-31 23:37:58 +0300

    Initial structure

---
+++ src/main/java/homebudget/HomebudgetApplication.java
@@ -0,0 +1,12 @@
+package homebudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomebudgetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomebudgetApplication.class, args);
+    }
+}
---
+++ src/test/java/homebudget/HomebudgetApplicationTests.java
@@ -0,0 +1,16 @@
+package homebudget;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = HomebudgetApplication.class)
+public class HomebudgetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
