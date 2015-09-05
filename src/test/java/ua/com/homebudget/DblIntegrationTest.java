0f7d24614162d8ff07ef0f5fd18074253e05bbc8
vgotsuliak <973gimf6>
2015-09-05 09:38:30 +0300

    Add dbl integration tests

---
+++ src/test/java/ua/com/homebudget/DblIntegrationTest.java
@@ -0,0 +1,24 @@
+package ua.com.homebudget;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.context.web.WebAppConfiguration;
+import ua.com.homebudget.configuration.TestAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {TestAppConfiguration.class})
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class})
+@ActiveProfiles(profiles = "test")
+@WebAppConfiguration
+public class DblIntegrationTest {
+}
