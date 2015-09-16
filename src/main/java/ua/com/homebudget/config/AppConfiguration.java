f3f6de1586bb6772a74d9e519ed42e0b74e7b1a2
unknown <ostefansky@gmail.com>
2015-09-17 01:37:55 +0300

    content from the static folder is accessible from app

--- src/main/java/ua/com/homebudget/config/AppConfiguration.java
+++ src/main/java/ua/com/homebudget/config/AppConfiguration.java
@@ -1,5 +1,6 @@
 package ua.com.homebudget.config;
 
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -13,9 +14,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableJpaRepositories(basePackages = {"ua.com.homebudget.repository"})
 @EnableTransactionManagement
 @ComponentScan("ua.com.homebudget")
-public class AppConfiguration extends WebMvcConfigurerAdapter {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("/", "classpath:/static/html/index.html");
-    }
+public class AppConfiguration extends WebMvcAutoConfigurationAdapter  {
+
 }
