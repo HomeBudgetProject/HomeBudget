507f130d2b3924e75c9e2bbead0b35c2086a42ae
Ruslan Kostrub <kostrub.r.v@gmail.com>
2015-10-21 17:27:11 +0300

    Imlemented login page. Refactored validation directives. Added page title change on url change.

--- src/main/resources/src/js/common/validationDirective.js
+++ src/main/resources/src/js/common/validationDirective.js
@@ -1,17 +1,19 @@
 angular.module('homebudgetApp').directive("validateemail", function() {
     return {
         restrict: 'A',
-        require: "ngModel",
-        link: function(scope, element, attributes, ngModel) {
+        require: ["ngModel", "^form"],
+        link: function(scope, element, attributes, requireArray) {
+            var ngModel = requireArray[0];
+            var form = requireArray[1];
             ngModel.$validators.required = function(modelValue) {
-                return (modelValue !== "") || scope.registerform.email.$pristine;
+                return (modelValue !== "") || form.email.$pristine;
             };
             ngModel.$validators.regexp = function(modelValue) {
                 var regexp = new RegExp("^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$");
-                return regexp.test(modelValue) || scope.registerform.email.$pristine;
+                return regexp.test(modelValue) || form.email.$pristine;
             };
             ngModel.$validators.emaillength = function(modelValue) {
-                return (modelValue && modelValue.length >= 6 && modelValue.length <= 60) || scope.registerform.email.$pristine;
+                return (modelValue && modelValue.length >= 6 && modelValue.length <= 60) || form.email.$pristine;
             };
         }
     };
@@ -19,17 +21,19 @@ angular.module('homebudgetApp').directive("validateemail", function() {
 angular.module('homebudgetApp').directive("validatepassword", function() {
     return {
         restrict: 'A',
-        require: "ngModel",
-        link: function(scope, element, attributes, ngModel) {
+        require: ["ngModel", "^form"],
+        link: function(scope, element, attributes, requireArray) {
+            var ngModel = requireArray[0];
+            var form = requireArray[1];
             ngModel.$validators.required = function(modelValue) {
-                return (modelValue !== "") || scope.registerform.password.$pristine;
+                return (modelValue !== "") || form.password.$pristine;
             };
             ngModel.$validators.passwordlength = function(modelValue) {
-                return (modelValue && modelValue.length >= 6 && modelValue.length <= 100) || scope.registerform.password.$pristine;
+                return (modelValue && modelValue.length >= 6 && modelValue.length <= 100) || form.password.$pristine;
             };
             ngModel.$validators.regexp = function(modelValue) {
                 var regexp = new RegExp("^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+$");
-                return regexp.test(modelValue) || scope.registerform.password.$pristine;
+                return regexp.test(modelValue) || form.password.$pristine;
             };
         }
     };
